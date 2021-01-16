faa96f5524c5103198cd51e1db3abf6fe40a1d2e
Nguyễn Quang Vinh <vinhng74@gmail.com>
2021-01-16 04:07:35 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log('example');
+
+var Mouse = require('./mouse')
+
+function Cat(){
+	this.stomach = [];
+};
+
+Cat.prototype.eat = function(mouse){
+	this.stomach.push(mouse);
+	mouse.die();
+}
+var Mickey = new Mouse('black');
+var jerry = new Mouse('orange');
+
+console.log(Mickey);
+console.log(jerry);
+
+var tom = new Cat();
+tom.eat(Mickey);
+tom.eat(jerry);
+console.log(tom);
+
+
